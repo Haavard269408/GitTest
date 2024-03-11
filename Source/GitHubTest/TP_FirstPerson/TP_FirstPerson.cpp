435a99157bcbfbed88cf3674dea932e3f1164c7c
Håvard_Jensen <142876336+Haavard269408@users.noreply.github.com>
2024-03-11 13:35:39 +0100

    Revert "test=1"
    This reverts commit 000c9639c66d76fb0cb90fa30205603756a02c17.

--- Source/GitHubTest/TP_FirstPerson/TP_FirstPerson.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TP_FirstPerson.h"
-#include "GitHubTest.h"
-
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP_FirstPerson, "TP_FirstPerson" );
- 
